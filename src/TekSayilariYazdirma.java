e53aa97d3b6803089c8b9abc0b179c22c06e07ab
sena <mskucukk@outlook.com>
2021-02-20 20:15:41 +0300

    1'den 99'a kadar tek sayılar

---
+++ src/TekSayilariYazdirma.java
@@ -0,0 +1,14 @@
+public class TekSayilariYazdirma {
+
+    public static void main(String args[]){
+
+        //1'den 99'a kadar olan tek sayıları yazdıran kodu giriniz.
+
+        for(int i =0 ; i < 100 ; i++ ){
+            if(i % 2 != 0){
+                System.out.println(i);
+            }
+        }
+
+    }
+}
